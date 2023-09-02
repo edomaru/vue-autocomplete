69c24489db5052c25f4c49542393f2e792975e9e
Eding Muhamad <masaruedo@yahoo.com>
2023-09-02 08:34:26 +0700

    Styling the components

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,11 @@ const country = ref('')
 </script>
 
 <template>
-  <div>
-    <h1>Selected {{ country }}</h1>
-    <Autocomplete :source="countries" v-model="country" />
+  <div class="w-screen h-screen bg-gray-200 flex flex-col justify-center items-center">
+    <div class="w-1/3">
+      <h1 class="text-center mb-3 font-bold">Selected {{ country }}</h1>
+      <Autocomplete :source="countries" v-model="country" />
+    </div>
   </div>
 </template>
 
